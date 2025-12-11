30e7272a2c1e922dba4dbc067afd9f0471fe4222
Kaif Arslan <110412833+KaifArslan@users.noreply.github.com>
2025-12-11 14:48:19 +0530

    Delete reservation-system directory

--- reservation-system/src/main/java/com/kaif/reservation_system/auth/RestaurantUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kaif.reservation_system.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface RestaurantUserRepository extends JpaRepository<RestaurantUser, Long> {
-    Optional<RestaurantUser> findByEmail(String email);
-}
--- reservation-system/src/main/java/com/kaif/reservation_system/repository/RestaurantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kaif.reservation_system.repository;
-
-import com.kaif.reservation_system.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-
-}
